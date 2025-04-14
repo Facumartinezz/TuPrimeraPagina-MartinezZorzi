90c7e1fdf502fd699099bfe00d5d5ec8baba8eab
Facumartinezz <facundomzorzi@hotmail.com>
2025-04-14 16:59:00 -0300

    Segundo push

---
+++ Proyecto1/PrimeraPagina/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Alumno, Profesor, Curso, Entregable
+
+def lista_alumnos(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'PrimeraPagina/lista_alumnos.html', {'alumnos': alumnos,})
+
+def detalle_alumno(request, pk):
+    alumno = get_object_or_404(Alumno, pk=pk)
+    return render(request, 'PrimeraPagina/detalle_alumno.html', {'alumno': alumno})
+
+def index(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'PrimeraPagina/index.html', {'alumnos': alumnos})
